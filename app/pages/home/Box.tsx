b906eca56a022b0d9e8472621b7d37339b5680a9
Fil Zembowicz <fil@formsort.com>
2020-10-03 20:29:29 -0400

    Try to import three.js SVGLoader

--- app/pages/home/Box.tsx
+++ app/pages/home/Box.tsx
@@ -2,6 +2,11 @@ import * as React from "react";
 import { useRef, useState } from "react";
 import { Canvas, useFrame } from "react-three-fiber";
 import { Vector3 } from "three";
+import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
+
+const SVG = {
+  email: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="18px" height="18px"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>`,
+};
 
 interface IBoxProps {
   position: Vector3;
@@ -24,6 +29,10 @@ export const Box: React.FunctionComponent<any> = (props) => {
     mesh.current.rotation.x += 0.01;
     mesh.current.rotation.y += 0.01;
   });
+
+  const loader = new SVGLoader();
+  // const svgData = loader.parse(SVG.email);
+  // console.log(svgData);
 
   return (
     <mesh
